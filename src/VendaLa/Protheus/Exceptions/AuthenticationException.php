be40f44a93211f02c9f53298227c4e9833c035a4
The Black MC (Jean C. Garcia) <jeancesargarcia@gmail.com>
2020-01-20 14:23:34 -0300

    auth exception

--- src/VendaLa/Protheus/Exceptions/AuthenticationException.php
+++ src/VendaLa/Protheus/Exceptions/AuthenticationException.php
@@ -3,6 +3,7 @@
 namespace VendaLa\Protheus\Exceptions;
 
 use Exception;
+use Throwable;
 use VendaLa\Protheus\Contracts\Authentication;
 
 /**
@@ -12,5 +13,19 @@ use VendaLa\Protheus\Contracts\Authentication;
  */
 class AuthenticationException extends Exception implements Authentication
 {
-
+    /**
+     * Construct the exception. Note: The message is NOT binary safe.
+     *
+     * @link  https://php.net/manual/en/exception.construct.php
+     *
+     * @param string    $message  [optional] The Exception message to throw.
+     * @param int       $code     [optional] The Exception code.
+     * @param Throwable $previous [optional] The previous throwable used for the exception chaining.
+     *
+     * @since 5.1.0
+     */
+    public function __construct($message = "", $code = 0, Throwable $previous = null)
+    {
+        parent::__construct($message, $code, $previous);
+    }
 }
