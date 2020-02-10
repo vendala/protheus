506779464044b44de7feff2d847ac103b9f1e36b
Filipe Bellezoni <bellezoni@gmail.com>
2020-02-10 16:06:38 -0300

    Estrutura de orders começando

--- src/VendaLa/Protheus/Resources/Orders.php
+++ src/VendaLa/Protheus/Resources/Orders.php
@@ -4,6 +4,9 @@ namespace VendaLa\Protheus\Resources;
 
 use VendaLa\Protheus\Contracts\ResourceFacotry;
 use VendaLa\Protheus\Helpers\Pagination;
+use VendaLa\Protheus\Protheus;
+
+use GuzzleHttp\Exception\RequestException;
 
 /**
  * Class Orders.
@@ -13,4 +16,46 @@ use VendaLa\Protheus\Helpers\Pagination;
 class Orders extends Pagination implements ResourceFacotry
 {
 
+    /**
+     * @const string
+     */
+    private const GET_PATH = 'rest/NO2W003';
+
+    /**
+     * @var array
+     */
+    private $body;
+
+    /**
+     * @var Protheus
+     */
+    private $protheus;
+
+    /**
+     * Products constructor.
+     *
+     * @param Protheus $protheus
+     */
+    public function __construct(Protheus $protheus)
+    {
+        $this->protheus = $protheus;
+    }
+
+    /**
+     * @return \GuzzleHttp\Psr7\Response|mixed|\Psr\Http\Message\ResponseInterface
+     *
+     * @throws OrdersException
+     */
+    public function get()
+    {
+        try {
+            $response = $this->protheus->post(self::GET_PATH, $this->body);
+            $response->getBody()->rewind();
+            return $response;
+        } catch (RequestException $requestException) {
+            ## TODO: Add log
+        } finally {
+            
+        }
+    }
 }
