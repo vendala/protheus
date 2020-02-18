7e0d50b3b3bcba4fa3686b3daaa2b38931d21b29
Filipe Bellezoni <bellezoni@gmail.com>
2020-02-18 17:41:38 -0300

    Removendo Class HttpRequest

--- src/VendaLa/Protheus/Exceptions/OrdersException.php
+++ src/VendaLa/Protheus/Exceptions/OrdersException.php
@@ -3,24 +3,20 @@
 namespace VendaLa\Protheus\Exceptions;
 
 use Throwable;
-use VendaLa\Protheus\Contracts\ProtheusExceptionContract;
+use \Exception;
+use \VendaLa\Protheus\Contracts\ProtheusExceptionContract;
 
 /**
  * Class AuthenticationException
  *
  * @package VendaLa\Protheus\Exceptions
  */
-class OrdersException extends ProtheusException implements ProtheusExceptionContract
+class OrdersException extends Exception implements ProtheusExceptionContract
 {
-    /**
-     * @const int
-     */
-    public const CODE = 2000;
-
     /**
      * @const string
      */
-    public const ORDER_DEFAULT = 'Erro ao buscar pedidos.';
+    public const ORDER_DEFAULT = 'Erro ao buscar os pedidos.';
 
     /**
      * @const string
@@ -28,9 +24,24 @@ class OrdersException extends ProtheusException implements ProtheusExceptionCont
     public const ORDER_SEND = 'Erro ao enviar o pedido';
 
     /**
+     * @const string
+     */
+    public const ORDER_GET = 'Erro ao buscar o pedido';
+
+    /**
+     * @const int
+     */
+    public const CODE_DEFAULT = 2000;
+
+    /**
      * @const int
      */
     public const CODE_SEND = 2001;
+
+    /**
+     * @const int
+     */
+    public const CODE_GET = 2002;
 
     /**
      * Construct the exception. Note: The message is NOT binary safe.
@@ -43,7 +54,7 @@ class OrdersException extends ProtheusException implements ProtheusExceptionCont
      *
      * @since 5.1.0
      */
-    public function __construct($message = self::ORDER_DEFAULT, $code = self::CODE, Throwable $previous = null)
+    public function __construct($message = self::ORDER_DEFAULT, $code = self::CODE_DEFAULT, Throwable $previous = null)
     {
         parent::__construct($message, $code, $previous);
     }
